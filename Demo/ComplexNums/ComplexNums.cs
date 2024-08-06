1ddf02883235abdf97237e2af6c440c2c2d34762
haji <muhamedhajajii@outlook.com>
2024-08-07 00:25:22 +0300

    Add project files.

---
+++ Demo/ComplexNums/ComplexNums.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.ComplexNums
+{
+    internal class ComplexNumss
+    {
+        public int Real { get; set; }
+        public int Imag { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i";
+        }
+
+        public static ComplexNumss operator +(ComplexNumss Left , ComplexNumss Right)
+        {
+            return new ComplexNumss()
+            {
+                // if there any value of the right and the left == null when it start propagate it may == null 
+                // null coalesecing operator works after the plus operator
+                Real = (Left?.Real ?? 0) + (Right?.Real ?? 0),
+                Imag = (Right?.Imag ?? 0) + (Left?.Imag ?? 0),
+            };
+        }
+
+        public static ComplexNumss operator -(ComplexNumss Left, ComplexNumss Right)
+        {
+            return new ComplexNumss()
+            {
+                Real = (Left?.Real ?? 0) - (Right?.Real ?? 0),
+                Imag = (Left?.Imag ?? 0) - (Right?.Imag ?? 0)
+            };
+        }
+
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,63 @@
+﻿using Demo.ComplexNums;
+
+namespace Demo
+{
+    internal class Program
+    {
+        /// Main Topics
+        /// 1- Opertator Overloading
+        /// 2- Casting Opertator Overloading
+        /// 3- Abstrication
+        /// 4- Static
+        /// 5- Sealed
+        /// 6- Partial
+
+        static void Main()
+        {
+            #region 1- Operator Overloading
+            /** Polymorphism [Many Forms]
+             * 1- Constructor overloading
+             * 2- indexer overloading
+             * 3- Method Overloading
+             * 4- Operator overloading
+             * 5- casting overloading
+             * Many Construct or methods or indexer with the same name with new behavior
+             * but keep separate them by the number and the type and the sequecne of the type
+             */
+
+            // Operator overloading
+
+            #region Binary Operator OverLoading
+            // complex numbers - real and imaginary
+            //ComplexNumss C1 = new ComplexNumss() {Real = 3,Imag = 5};
+            //Console.WriteLine(C1); // 3 + 5i
+
+            //ComplexNumss C2 = new ComplexNumss() { Real = 2, Imag = 4 };
+            ////ComplexNumss C2 = default;
+            //Console.WriteLine(C2);
+
+            //ComplexNumss C3 = C1 + C2; // Invalid Because the plus cannot multiply within two objects 
+            //Console.WriteLine(C3); // 5 + 9i
+            //// the operator at the end still only a function 
+            //// it Must be static a class memeber function can call it from the class and must be public
+            //// uniry operators
+            //// relational operatora
+
+            //ComplexNumss C4 = C1 - C2;
+            //Console.WriteLine(C4);
+
+            //ComplexNumss C5 = new ComplexNumss()
+            //{
+            //    Real = 3,Imag= 5
+            //};
+            //C5 += C3;
+            #endregion
+
+
+
+
+
+            #endregion
+        }
+    }
+}
